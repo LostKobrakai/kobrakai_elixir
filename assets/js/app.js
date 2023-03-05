6ff90b87c9bba190ac1f81e6d9be762fd2c235f9
Benjamin Milde <benni@kobrakai.de>
2023-03-05 20:50:19 +0100

    Simplify darkmode JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,27 +48,22 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
-function isDarkPrefered() {
-  return window.matchMedia('(prefers-color-scheme: dark)').matches
+function darkExpected() {
+  return localStorage.theme === 'dark' || (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches);
 }
 
 function initDarkMode() {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && isDarkPrefered())) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
+  if (darkExpected()) document.documentElement.classList.add('dark');
+  else document.documentElement.classList.remove('dark');
 }
 
-initDarkMode()
-
 window.addEventListener("toogle-darkmode", e => {
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && isDarkPrefered())) {
-    localStorage.theme = 'light'
-  } else {
-    localStorage.theme = 'dark'
-  }
-  initDarkMode()
+  if (darkExpected()) localStorage.theme = 'light';
+  else localStorage.theme = 'dark';
+  initDarkMode();
 })
+
+initDarkMode();
 
