d4247bd393270e17aa32b6bb6c4bffe242c55625
Benjamin Milde <benni@kobrakai.de>
2024-08-17 18:47:35 +0200

    Make websocket url relative

--- lib/kobrakai_web/live/websocket.ex
+++ lib/kobrakai_web/live/websocket.ex
@@ -4,11 +4,7 @@ defmodule KobrakaiWeb.Websocket do
   @impl true
   def render(assigns) do
     ~H"""
-    <div
-      id="websocket"
-      phx-hook="Websocket"
-      data-url={url(@socket, ~p"/ws/connection_timer/blogpost")}
-    >
+    <div id="websocket" phx-hook="Websocket" data-url={~p"/ws/connection_timer/blogpost"}>
       <span data-tag="init">Initializing...</span>
       <.button phx-click={JS.dispatch("request")}>Send request</.button>
       <ol class="max-h-48 overflow-x overflow-y-auto" data-tag="list"></ol>
