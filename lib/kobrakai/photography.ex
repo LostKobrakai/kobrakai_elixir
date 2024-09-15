239cca7e27dce8fca15ba480ab489702db0a183b
Benjamin Milde <benni@kobrakai.de>
2024-09-15 16:20:54 +0200

    Switch out pictures

--- lib/kobrakai/photography.ex
+++ lib/kobrakai/photography.ex
@@ -1,25 +1,21 @@
 defmodule Kobrakai.Photography do
   def all_images do
     [
+      "20220929-Bild010_Neg.Nr.9.jpg",
       "20150819-_ben0528.jpg",
       "20150416-_ben9024-bearbeitet.jpg",
+      "20150121-023_20-2.jpg",
       "20150819-_ben0257-bearbeitet.jpg",
-      "ben7148-bearbeitet.jpg",
-      "ben7320-bearbeitet.jpg",
+      "20141222-_BEN7840.jpg",
       "20140404-_ben6885-bearbeitet.jpg",
-      "katharina_5.jpg",
-      "20130903-_ben1307.jpg",
       "20130705-_ben0348-bearbeitet.jpg",
-      "20130705-_ben0141.jpg",
-      "20130516-_ben9379.jpg",
-      "ben8448-bearbeitet.jpg",
       "ben8565-bearbeitet.jpg",
+      "20130516-_ben9379.jpg",
+      "20140429-_BEN6983.jpg",
       "20130322-_ben7772-bearbeitet.jpg",
-      "20120125-beni3803.jpg",
-      "20130322-_ben7876.jpg",
       "20130322-_ben8030.jpg",
-      "20120329-_ben4623.jpg",
-      "20120501-_ben5353.jpg"
+      "20120820-_BEN6875.jpg",
+      "20150121-003_0-2.jpg"
     ]
   end
 
@@ -27,10 +23,10 @@ defmodule Kobrakai.Photography do
     [
       "20150819-_ben0528.jpg",
       "20150819-_ben0257-bearbeitet.jpg",
-      "ben7148-bearbeitet.jpg",
-      "ben7320-bearbeitet.jpg",
+      "20150121-023_20-2.jpg",
       "20140404-_ben6885-bearbeitet.jpg",
-      "20130705-_ben0348-bearbeitet.jpg"
+      "20130705-_ben0348-bearbeitet.jpg",
+      "20130322-_ben8030.jpg"
     ]
   end
 end
