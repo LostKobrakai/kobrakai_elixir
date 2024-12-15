ffdbec90efa4f3853141c83632becec9964a2f0a
Benjamin Milde <benni@kobrakai.de>
2024-12-15 13:19:30 +0100

    Fix tests for changed route

--- test/kobrakai_web/controllers/video_controller_test.exs
+++ test/kobrakai_web/controllers/video_controller_test.exs
@@ -7,11 +7,11 @@ defmodule KobrakaiWeb.VideoControllerTest do
         Req.Test.json(conn, %{data: [%{id: "12345", published_at: "2024-11-05T10:00:00"}]})
       end)
 
-      conn = get(conn, ~p"/video")
+      conn = get(conn, ~p"/videos")
 
       assert html = html_response(conn, 200)
 
-      assert html =~ ~p"/video/12345"
+      assert html =~ ~p"/videos/12345"
     end
   end
 end
