c8593ecb822d5d1c5d7a00bd3cf5a2c945b36616
Benjamin Milde <benni@kobrakai.de>
2024-12-26 15:48:13 +0100

    Put videos on homepage

--- lib/kobrakai_web/controllers/page_controller.ex
+++ lib/kobrakai_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule KobrakaiWeb.PageController do
   use KobrakaiWeb, :controller
 
   def home(conn, _params) do
+    videos =
+      Kobrakai.Bold.list_videos!().body["data"]
+      |> Enum.sort_by(& &1["published_at"])
+      |> Enum.take(4)
+      |> Enum.map(&Kobrakai.Bold.video_response_mapping/1)
+      |> Stream.concat(Stream.repeatedly(fn -> nil end))
+      |> Enum.take(4)
+
     # The home page is often custom made,
     # so skip the default app layout.
     conn
@@ -9,7 +17,8 @@ defmodule KobrakaiWeb.PageController do
     |> render(:home,
       projects: Kobrakai.Portfolio.featured_projects(),
       posts: Kobrakai.Blog.all_posts(),
-      images: Kobrakai.Photography.homepage()
+      images: Kobrakai.Photography.homepage(),
+      videos: videos
     )
   end
 end
